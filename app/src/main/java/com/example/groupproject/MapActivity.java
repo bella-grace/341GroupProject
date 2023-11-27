1f608d132566b8033a6bee8d6072d3c4475545b0
bella-grace <igmendoza2003@gmail.com>
2023-11-27 00:35:57 -0800

    Finished carrot and profile page, added first btn to map

--- app/src/main/java/com/example/groupproject/MapActivity.java
+++ app/src/main/java/com/example/groupproject/MapActivity.java
@@ -1,15 +1,28 @@
 package com.example.groupproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
 
+    ImageButton toCarrot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        toCarrot = findViewById(R.id.toCarrot);
+        toCarrot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent showCarrot = new Intent(MapActivity.this, ProductActivity1.class);
+                startActivity(showCarrot);
+            }
+        });
     }
 
 
