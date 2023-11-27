6ad790a7945f11c44623720a8a8e2342687a48c9
laurajmoore <149132172+laurajmoore@users.noreply.github.com>
2023-11-26 17:01:41 -0800

    Creating adapter

--- app/src/main/java/com/example/groupproject/HelpMainActivity.java
+++ app/src/main/java/com/example/groupproject/HelpMainActivity.java
@@ -3,6 +3,8 @@ package com.example.groupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class HelpMainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HelpMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_main);
+        Spinner spinner=findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.my_array,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 }
