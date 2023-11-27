0b89a2de83feeb4dcb41b9106e7d2b20c620a689
laurajmoore <149132172+laurajmoore@users.noreply.github.com>
2023-11-26 17:49:52 -0800

    Making buttons do something

--- app/src/main/java/com/example/groupproject/HelpMainActivity.java
+++ app/src/main/java/com/example/groupproject/HelpMainActivity.java
@@ -2,8 +2,11 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class HelpMainActivity extends AppCompatActivity {
@@ -18,6 +21,14 @@ public class HelpMainActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        Button button = (Button) findViewById(R.id.q1_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpMainActivity.this, HelpMainActivity2.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
