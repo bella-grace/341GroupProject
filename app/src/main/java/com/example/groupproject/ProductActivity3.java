874d69355cb84f29eb2c67f597f3384491a6fc48
bella-grace <igmendoza2003@gmail.com>
2023-11-27 11:44:26 -0800

    Added msg screen

--- app/src/main/java/com/example/groupproject/ProductActivity3.java
+++ app/src/main/java/com/example/groupproject/ProductActivity3.java
@@ -5,27 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class ProductActivity2 extends AppCompatActivity {
 
-    ImageButton toProductPage;
+public class ProductActivity3 extends AppCompatActivity {
 
+    ImageButton toProduct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product2);
+        setContentView(R.layout.activity_product3);
 
-        toProductPage = findViewById(R.id.backMain3);
+        toProduct = findViewById(R.id.backMain2);
 
-        toProductPage.setOnClickListener(new View.OnClickListener() {
+        toProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toProduct = new Intent(ProductActivity2.this, ProductActivity1.class);
+                Intent toProduct = new Intent(ProductActivity3.this, ProductActivity1.class);
                 startActivity(toProduct);
             }
         });
+
 
     }
 }
