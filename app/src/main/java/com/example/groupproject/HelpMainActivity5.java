cb3777d40434df9680428627960b9927c87d42c3
laurajmoore <149132172+laurajmoore@users.noreply.github.com>
2023-11-26 17:16:57 -0800

    Adding other activity pages

---
+++ app/src/main/java/com/example/groupproject/HelpMainActivity5.java
@@ -0,0 +1,14 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpMainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_main5);
+    }
+}
